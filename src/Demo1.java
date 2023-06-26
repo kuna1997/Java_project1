703137517f3c82adab46d92efb95d1e50477f3b9
kuna1997 <131571441+kuna1997@users.noreply.github.com>
2023-06-26 17:52:52 +0530

    Update Demo1.java

--- src/Demo1.java
+++ src/Demo1.java
@@ -3,6 +3,10 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		System.out.println("This is the main method");
+		System.out.println("This is kunal");
+		System.out.println("pc");
+		
+		
 
 	}
 
